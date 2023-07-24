ce0fb637e927615ddca8cde2b02922fbf10152f0
John Nolan <john.nolan@methods.co.uk>
2023-07-24 12:27:56 +0100

    Fix es6 export issue

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ const config = {
     reporters: ['default', 'jest-junit'],
     resetMocks: true,
     transform: {
-        '\\.[jt]sx?$': 'babel-jest',
+        '\\.js?$': 'babel-jest',
     },
     testMatch: ['**/?(*.)+(test).js'],
     collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**', '!**/vendor/**'],
 };
 
-module.exports = config;
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* istanbul ignore file */
-require('es6-promise/auto');
 import { initAll } from 'govuk-frontend';
 import jsEnabled from './scripts/jsEnabled';
 
